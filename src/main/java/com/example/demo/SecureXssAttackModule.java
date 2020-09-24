dc4546e89ad2607a186338e4bfb01b1ca1963c20
sahilllSharma <49686087+sahilllSharma@users.noreply.github.com>
2020-09-24 19:37:28 +0530

    SecureXssModule added

---
+++ src/main/java/com/example/demo/SecureXssAttackModule.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+public class SecureXssAttackModule {
+
+	private int oauth;
+	private String passcode;
+	private float key;
+	public int getOauth() {
+		return oauth;
+	}
+	public void setOauth(int oauth) {
+		this.oauth = oauth;
+	}
+	public String getPasscode() {
+		return passcode;
+	}
+	public void setPasscode(String passcode) {
+		this.passcode = passcode;
+	}
+	public float getKey() {
+		return key;
+	}
+	public void setKey(float key) {
+		this.key = key;
+	}
+	@Override
+	public String toString() {
+		return "SecureXssAttackModule [oauth=" + oauth + ", passcode=" + passcode + ", key=" + key + "]";
+	}
+	
+}
